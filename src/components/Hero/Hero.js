ed6f31eab9fb82c4ecfff3eca9a16c189c3c7897
ssuvorov <sergey.suvorov@mercedes-benz.com>
2024-03-24 17:57:40 +0100

    css visuals

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,10 @@ function Hero({ children, spline, img = "" }) {
   return (
     <>
       <div className="relative mx-auto my-4 flex min-h-[600px] flex-col-reverse items-center justify-between px-2 md:flex-row md:pt-0">
-        <div className="flex items-center md:w-3/6 md:pr-5">
+        <div className="z-10 flex items-center md:w-3/6 md:pr-5">
           <div className="text-left">{children}</div>
         </div>
-        <div className="grid h-[400px] w-full items-stretch overflow-hidden md:h-[700px] md:w-3/6">
+        <div className="grid h-[500px] w-full items-stretch md:h-[700px] md:w-3/6">
           {img && <img src={img} width={"60%"} className={"mx-auto "} />}
           {spline && <Spline scene={spline} />}
         </div>
