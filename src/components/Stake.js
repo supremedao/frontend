5baee0d50050f9b0803ac4c86f895255cb05625e
ssuvoro <s.suvoroff@gmail.com>
2024-07-19 17:05:43 +0200

    feat: UI

--- src/components/Stake.js
+++ src/components/Stake.js
@@ -146,7 +146,7 @@ export function Stake() {
                 />
                 <div className="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:size-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
                 <Typography className={"ms-3 font-medium "}>
-                  Stake with Keeper
+                  Stake with keeper to save on gas
                 </Typography>
               </label>
               <div className="ml-4">
